33e94766b1c5b1d0e966ea70bad2f93e0ad5487c
ahmed7761 <sharif7761@gmail.com>
2023-12-14 12:14:25 +0600

    user schema

---
+++ src/models/userModel.js
@@ -0,0 +1,57 @@
+const {Schema, model} = require('mongoose')
+const bcrypt = require('bcrypt')
+const {defaultImagePath} = require('../secret')
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'User name is required'],
+        trim: true,
+        minLength: [3, 'Name must be more than 3 characters'],
+        maxLength: [30, 'Name must be less than 31 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'User email is required'],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        minLength: [3, 'Name must be more than 3 characters'],
+        maxLength: [30, 'Name must be less than 31 characters'],
+        validate: {
+            validator:  function(email) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+            },
+            message: "please enter a valid email"
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'User password is required'],
+        minLength: [6, 'password must be more than 6 characters'],
+        set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10))
+    },
+    image: {
+        type: String,
+        default: defaultImagePath
+    },
+    address: {
+        type: String,
+        required: [true, 'User address is required'],
+    },
+    phone: {
+        type: String,
+        required: [true, 'User phone is required'],
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isBanned: {
+        type: Boolean,
+        default: false
+    },
+}, {timestamps: true})
+
+const User = model('Users', userSchema)
+module.exports = User
--- src/secret.js
+++ src/secret.js
@@ -2,4 +2,6 @@ require('dotenv').config()
 const serverPort = process.env.SERVER_PORT || 3002
 const mongodbURL = process.env.MONGODB_URL || "mongodb://localhost:27017/ecomMERNDB"
 
-module.exports = {serverPort, mongodbURL}
+const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH || 'public/images/users/user-default.jpg'
+
+module.exports = {serverPort, mongodbURL, defaultImagePath}
