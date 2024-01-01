24b0977c8777168f0a8e8096c1292ddde5161ec6
ahmed7761 <sharif7761@gmail.com>
2024-01-01 23:17:26 +0600

    file filtering

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,8 +1,11 @@
 const multer = require('multer')
 const path = require('path')
 const {uploadDirectory} = require("../secret");
+const CreateError = require('http-errors')
 
 const UPLOAD_DIR = uploadDirectory
+const MAX_FILE_SIZE = Number(process.env.MAX_FILE_SIZE) || 1024 * 1024 * 2
+const ALLOWED_FILE_TYPES = process.env.ALLOWED_FILE_TYPES || ['jpg','jpeg','png']
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, UPLOAD_DIR)
@@ -14,6 +17,17 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: storage })
+const fileFilter = (req, file, cb) => {
+    const extname = path.extname(file.originalname)
+    if(!ALLOWED_FILE_TYPES.includes(extname.substring(1))){
+        return cb(CreateError(400, 'File format not allowed'))
+    }
+    cb(null, true)
+}
+const upload = multer({
+    storage: storage,
+    limits: {fileSize : MAX_FILE_SIZE},
+    fileFilter
+})
 
 module.exports = upload;
